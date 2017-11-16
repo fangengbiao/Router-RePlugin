a91f0093d06f5f6b199080083e4e13583d57e4ed
gfan <gengbiao.fan.o@nio.com>
2017-11-16 15:33:07 +0800

    缩减提供插件 jar 运行成功

--- RePluginDemo/usercenter/src/main/java/com/lzh/usercenetr/LoginActivity.java
+++ RePluginDemo/usercenter/src/main/java/com/lzh/usercenetr/LoginActivity.java
@@ -3,13 +3,12 @@ package com.lzh.usercenetr;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.lzh.compiler.parceler.Parceler;
-import com.lzh.compiler.parceler.annotation.Arg;
+import com.nio.compiler.parceler.Parceler;
+import com.nio.compiler.parceler.annotation.Arg;
 import com.lzh.nonview.router.anno.RouterRule;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 @RouterRule("login")
@@ -20,9 +19,9 @@ public class LoginActivity extends Activity {
     @Arg
     String password;
 
-    @BindView(R.id.username)
-    EditText name;
-    @BindView(R.id.password)
+//    @BindView(R.id.username)
+//    EditText name;
+//    @BindView(R.id.password)
     EditText word;
 
     @Override
@@ -30,9 +29,12 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Parceler.toEntity(this, getIntent());
-        ButterKnife.bind(this);
-        name.setText(username);
-        word.setText(password);
+//        ButterKnife.bind(this);
+//        name.setText(username);
+//        word.setText(password);
+
+        TextView textView = (TextView) this.findViewById(R.id.username);
+        textView.setText(username);
     }
 
     @OnClick(R.id.login)
